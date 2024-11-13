83c7416de343c5fd25e7a0567f25d4845852ec1c
Uyooghene <uyooghene01@gmail.com>
2024-11-13 07:34:37 +0100

    new campground

--- app.js
+++ app.js
@@ -20,14 +20,37 @@ const app = express();
 app.set('view engine', 'ejs')
 app.set('views',path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended : true}))
+
 app.get('/', (req,res) => {
     res.send('hello from yelpcamp')
 })
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title: 'My backyard', description: 'testing camp'})
-    await camp.save();
-    res.send(camp)
+
+app.get('/campgrounds', async (req,res) => {
+    const campgrounds = await Campground.find({})
+    res.render('./campgrounds/index.ejs', {campgrounds} )
 })
+app.get('/campgrounds/new', (req,res) => {
+    res.render('./campgrounds/new.ejs' )
+})
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get('/campgrounds/:id', async (req,res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('./campgrounds/show.ejs', {campground} )
+})
+
+
+
+// app.get('/makecampground', async (req, res) => {
+//     const camp = new Campground({title: 'My backyard', description: 'testing camp'})
+//     await camp.save();
+//     res.send(camp)
+// })
 
 
 app.listen(3000, () => {
