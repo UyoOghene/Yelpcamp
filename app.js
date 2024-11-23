16caf93cafec62aed426cab1b1f58f2744f099c7
Uyooghene <uyooghene01@gmail.com>
2024-11-23 09:53:24 +0100

    g

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.set('views',path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended : true}))
 app.use(methodOverride('_method'))
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     res.send('hello from yelpcamp')
@@ -42,10 +43,26 @@ app.post('/campgrounds', async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 });
 
-app.get('/campgrounds/:id', async (req,res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('./campgrounds/show.ejs', {campground} )
-})
+// app.get('/campgrounds/:id', async (req,res) => {
+//     const campground = await Campground.findById(req.params.id)
+//     res.render('./campgrounds/show.ejs', {campground} )
+// })
+app.get('/campgrounds/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).send("Invalid Campground ID");
+        }
+        const campground = await Campground.findById(id);
+        if (!campground) {
+            return res.status(404).send("Campground not found");
+        }
+        res.render('./campgrounds/show.ejs', { campground });
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("Something went wrong");
+    }
+});
 
 app.get('/campgrounds/:id/edit', async (req,res) => {
     const campground = await Campground.findById(req.params.id)
