241671eadc68959270c465ba4e070d1351e1651b
Uyooghene <uyooghene01@gmail.com>
2025-01-09 22:26:13 +0100

    register

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-
+const catchAsync = require('../utilities/catchAsync')
 router.get('/register', (req,res)=>{
     res.render('users/register')
 })
 
-router.post('/register',async (req,res) => {
+router.post('/register', catchAsync(async (req,res) => {
     const {email, username, password} = req.body;
     const user = new User({email,username})
     const registeredUser = await User.register(user, password)
     console.log(registeredUser);
     req.flash('success','welcome to yelpcamp');
     res.redirect('/campgrounds')
-})
+}));
 module.exports = router;
