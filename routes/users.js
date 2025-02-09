2aeadf32c206f82a4da9f681fe098716ba4124b3
Uyooghene <uyooghene01@gmail.com>
2025-02-09 12:50:37 +0100

    router.route

--- routes/users.js
+++ routes/users.js
@@ -6,39 +6,18 @@ const users = require('../controllers/users');
 
 const catchAsync = require('../utilities/catchAsync');
 
-router.get('/register',users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register))
 
-// router.post('/register', catchAsync(async (req,res) => {
-//     try {
-//     const {email, username, password} = req.body;
-//     const user = new User({email,username})
-//     const registeredUser = await User.register(user, password)
-//     console.log(registeredUser);
-//     req.flash('success','welcome to yelpcamp');
-//     res.redirect('/campgrounds')
-//     }catch(e){
-//         req.flash('error', e.message);
-//         res.redirect('/register');
-//     }
-// }));
-
-router.get('/login', users.renderLogin);
-
-// router.post('/login', passport.authenticate('local',{failureFlash: true, failureRedirect: '/login'}, (req,res)=>{
-//     req.flash('success', 'welcome to yelpcamp');
-//     res.redirect('/campgrounds');
-// }));
-router.post('/register', catchAsync(users.register));
-
-router.post('/login', passport.authenticate('local', {
-    failureFlash: true,
-    failureRedirect: '/login'
-}), users.login);
+router.route('/login')
+    .get( users.renderLogin)
+    .post(passport.authenticate('local', {
+        failureFlash: true,
+        failureRedirect: '/login'
+    }), users.login);
+    
 
 router.get('/logout',users.logout); 
-
-
-
-
 
 module.exports = router;
