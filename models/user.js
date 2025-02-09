712a7ac809df0a4d607770cb21df729c1fc6c225
Uyooghene <uyooghene01@gmail.com>
2025-02-09 12:34:25 +0100

    logout controller

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
+
 const UserSchema = new Schema({
    email: {
         type: String,
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const passport = require('passport');
+const users = require('../controllers/users');
+
 const catchAsync = require('../utilities/catchAsync');
-router.get('/register', (req,res)=>{
-    res.render('users/register')
-})
+
+router.get('/register',users.renderRegister);
 
 // router.post('/register', catchAsync(async (req,res) => {
 //     try {
@@ -21,52 +22,20 @@ router.get('/register', (req,res)=>{
 //     }
 // }));
 
-router.get('/login', (req,res)=>{
-    res.render('users/login')
-})
+router.get('/login', users.renderLogin);
 
 // router.post('/login', passport.authenticate('local',{failureFlash: true, failureRedirect: '/login'}, (req,res)=>{
 //     req.flash('success', 'welcome to yelpcamp');
 //     res.redirect('/campgrounds');
 // }));
-router.post('/register', catchAsync(async (req, res, next) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        console.log(registeredUser);
-        req.login(registeredUser,err =>{
-            if(err){
-                return next(err)
-            }
-        });
-        req.flash('success', 'Welcome to YelpCamp!');
-        res.redirect('/campgrounds');
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/login');
-    }
-}));
+router.post('/register', catchAsync(users.register));
 
 router.post('/login', passport.authenticate('local', {
     failureFlash: true,
     failureRedirect: '/login'
-}), (req, res) => {
+}), users.login);
 
-    req.flash('success', 'Welcome back to YelpCamp!');
-    const redirectUrl = req.session.returnTo || '/campgrounds'
-    res.redirect(redirectUrl);
-});
-
-router.get('/logout', (req, res, next) => {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success', 'Goodbye!');
-        res.redirect('/campgrounds');
-    });
-}); 
+router.get('/logout',users.logout); 
 
 
 
