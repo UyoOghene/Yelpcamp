7d93309a28ab278b20b50ddf93a961191b3e87c3
Uyooghene <uyooghene01@gmail.com>
2025-02-24 09:27:31 +0100

    carousel working

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -13,8 +13,6 @@ const upload = multer({ storage});
 
 
 
-router.route('/')
-    .get( catchAsync(campgrounds.index))
     // .post( isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
     router.route('/')
     .get(catchAsync(campgrounds.index))
@@ -25,9 +23,9 @@ router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.editForm))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground)) 
     .delete( isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
-
+    
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
 module.exports = router;
